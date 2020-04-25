0aa751e89293f24a14855691b2046aa83a723311
balinux <Ryojuniyantara@gmail.com>
2020-04-25 10:13:38 +0800

    update: remove container

--- lib/main.dart
+++ lib/main.dart
@@ -39,19 +39,14 @@ class _MyHomePageState extends State<MyHomePage> {
             fit: BoxFit.cover,
           ),
           Container(
-            height: 300,
-            width: 200,
-          ),
-          Container(
             height: 200.0,
             width: MediaQuery.of(context).size.width,
             decoration: BoxDecoration(
                 gradient: LinearGradient(
-                    begin: FractionalOffset.topCenter,
-                    end: FractionalOffset.bottomCenter,
-                    colors: [Colors.grey.withOpacity(0.0), Colors.black],
-                    stops: [0.0, 1.0])),
-            // alignment: Alignment.bottomCenter,
+              begin: FractionalOffset.topCenter,
+              end: FractionalOffset.bottomCenter,
+              colors: [Colors.grey.withOpacity(0.0), Colors.black],
+            )),
             child: Column(
               children: <Widget>[
                 Container(
@@ -60,37 +55,39 @@ class _MyHomePageState extends State<MyHomePage> {
                     "Enjoy every moment with us",
                     textAlign: TextAlign.center,
                     style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 24,
-                        fontFamily: "PlayfairDisplayRegular"),
+                      fontSize: 28.0,
+                      color: Colors.white,
+                      fontFamily: "PlayfairDisplayRegular",
+                    ),
                   ),
                 ),
                 SizedBox(
                   height: 20.0,
                 ),
                 ButtonTheme(
-                    minWidth: 100.0,
-                    height: 50.0,
-                    child: FlatButton(
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18.0)),
-                      onPressed: () {},
-                      color: Colors.red[300],
-                      child: Text(
-                        "Sign in",
-                        style: TextStyle(fontFamily: "PoppinsRegular"),
-                      ),
-                      textColor: Colors.white,
-                    )),
+                  minWidth: 100.0,
+                  height: 50.0,
+                  child: FlatButton(
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18.0)),
+                    color: Colors.red[300],
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                          color: Colors.white, fontFamily: "PoppinsRegular"),
+                    ),
+                  ),
+                ),
                 SizedBox(
                   height: 20.0,
                 ),
                 Text(
                   "create account",
                   style: TextStyle(
+                      fontFamily: "PoppinsRegular",
                       color: Colors.white,
-                      decoration: TextDecoration.underline,
-                      fontFamily: "PoppinsRegular"),
+                      decoration: TextDecoration.underline),
                 )
               ],
             ),
